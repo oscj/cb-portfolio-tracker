3a1b0e7eb2be3689e7569ca2ccd39247bc7f8e43
oscjaimes <oscar@jaimes.io>
2021-05-01 20:55:36 -0600

    added dart application. Split up API and app

---
+++ portfoliotracker/android/app/src/main/kotlin/com/example/portfoliotracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portfoliotracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ portfoliotracker/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coinbase Portfolio Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coinbase Portfolio Tracker"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(),
+    );
+  }
+}
