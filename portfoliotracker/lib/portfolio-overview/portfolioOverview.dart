507a1675617490e8807b4879a547de8cbcf1423f
oscjaimes <oscar@jaimes.io>
2021-05-04 21:02:20 -0600

    implemented basic card to show stats

--- portfoliotracker/lib/portfolio-overview/portfolioOverview.dart
+++ portfoliotracker/lib/portfolio-overview/portfolioOverview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfoliotracker/shared/statCard.dart';
+import 'package:portfoliotracker/shared/colors.dart' as colorPallete;
 
 class PortfolioOverview extends StatefulWidget {
   @override
@@ -8,6 +10,27 @@ class PortfolioOverview extends StatefulWidget {
 class _PortfolioOverviewState extends State<PortfolioOverview> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Container(
+              child: Column(
+                children: [
+                  StatCard(
+                      title: "Total Investments",
+                      content: "\$69.420",
+                      contentColor: colorPallete.Colors.mainDark),
+                  StatCard(
+                      title: "Total Portfolio Value",
+                      content: "\$69.420",
+                      contentColor: colorPallete.Colors.mainDark),
+                  StatCard(
+                      title: "All Time Portfolio P/L",
+                      content: "\$69.420",
+                      contentColor: Colors.green)
+                ],
+              ),
+            )));
   }
 }
--- portfoliotracker/lib/shared/colors.dart
+++ portfoliotracker/lib/shared/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class Colors {
   static const mainDark = Color.fromRGBO(0, 46, 44, 1);
+  static const blueCream = Color.fromRGBO(197, 214, 216, 1);
 }
