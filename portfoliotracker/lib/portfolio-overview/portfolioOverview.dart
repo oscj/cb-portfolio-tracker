0d2af47d7ce6909de1b8fe15a127e9368ea0cdcb
oscjaimes <oscar@jaimes.io>
2021-05-04 21:21:22 -0600

    added margins to widget, updated readme

--- portfoliotracker/lib/portfolio-overview/portfolioOverview.dart
+++ portfoliotracker/lib/portfolio-overview/portfolioOverview.dart
@@ -12,11 +12,21 @@ class _PortfolioOverviewState extends State<PortfolioOverview> {
   Widget build(BuildContext context) {
     return Container(
         child: Container(
+            margin: EdgeInsets.all(10),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.5,
             child: Container(
               child: Column(
                 children: [
+                  Center(
+                    child: Text(
+                      "Portfolio Overview",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: colorPallete.Colors.mainDark),
+                    ),
+                  ),
                   StatCard(
                       title: "Total Investments",
                       content: "\$69.420",
