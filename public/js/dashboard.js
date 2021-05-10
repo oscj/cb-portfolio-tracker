7e80b6ef8c631546921308e9f751a0783d1e44b7
oscjaimes <oscar@jaimes.io>
2021-05-09 22:43:42 -0600

    Added functionality to load list of accounts (with funds witihin them)

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,7 @@
 let portfolioValueObject;
+let accountsObject;
 
 const populatePortfolioValue = () => {
-    console.log("yes");
     $.ajax({
         type: 'get',
         url: "http://localhost:3007/account-info/portfolio-value",
@@ -20,9 +20,33 @@ const populatePortfolioValue = () => {
     });
 };
 
+const populateAccountList = () => {
+    $.ajax({
+        type: 'get',
+        url: "http://localhost:3007/account-info/accounts",
+        cache: false,
+        async: 'asynchronous',
+        success: function (accountsResponse) {
+            accountsObject = accountsResponse;
+            accounts = accountsObject.wallet;
+            let htmlStr = "";
+            for (coin in accounts) {
+                if (Number(accounts[coin].amount) > 0) {
+                    htmlStr += `<li class="p-4 hover:bg-gray-50 cursor-pointer font-bold text-blue-800">${accounts[coin].currency}</li>`
+                }
+            }
+            document.getElementById('account-list').innerHTML = htmlStr;
+        },
+        error: function (request, status, error) {
+            alert("Couldn't fetch account data");
+        }
+    });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     // Call all necessary population functions
     populatePortfolioValue();
+    populateAccountList();
     // set functions to execute and update data every 1 min
     window.setInterval(() => {
         populatePortfolioValue();
