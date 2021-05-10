cee96961726ed18bcfc3b06c71b9c0b14ed8344b
oscjaimes <oscar@jaimes.io>
2021-05-09 22:00:04 -0600

    implemented data update every 1 min

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    populatePortfolioValue();
-});
+let portfolioValueObject;
 
 const populatePortfolioValue = () => {
+    console.log("yes");
     $.ajax({
         type: 'get',
         url: "http://localhost:3007/account-info/portfolio-value",
         cache: false,
         async: 'asynchronous',
-        success: function (json) {
-            document.getElementById('portfolio-value').innerHTML = json.totalBalance;
-            document.getElementById('portfolio-currency').innerHTML = json.nativeCurrency;
+        success: function (portfolioValueResponse) {
+            // cache portfolio value object response
+            portfolioValueObject = portfolioValueResponse;
+            document.getElementById('portfolio-value').innerHTML = portfolioValueResponse.totalBalance;
+            document.getElementById('portfolio-currency').innerHTML = portfolioValueResponse.nativeCurrency;
         },
         error: function (request, status, error) {
             alert("Couldn't fetch portfolio value");
@@ -18,3 +19,12 @@ const populatePortfolioValue = () => {
         }
     });
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Call all necessary population functions
+    populatePortfolioValue();
+    // set functions to execute and update data every 1 min
+    window.setInterval(() => {
+        populatePortfolioValue();
+    }, 60000);
+});
